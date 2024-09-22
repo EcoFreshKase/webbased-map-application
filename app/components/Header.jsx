30a37e9f97e1581ed773a690e6ba6be6c0b74e8f
Anni <annika.schatter@web.de>
2024-09-22 23:17:05 +0200

    added a header

---
+++ app/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header>
+          <h1>
+            Welcome to this
+          </h1>
+          <h2>web-based map application</h2>
+        </header>
+    )
+}
